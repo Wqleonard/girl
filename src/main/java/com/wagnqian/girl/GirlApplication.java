cf2c52cca06e31b482f411ff055c9c2e1109f14f
T1 <>
2018-02-28 13:26:35 +0800

    Initial commit

---
+++ src/main/java/com/wagnqian/girl/GirlApplication.java
@@ -0,0 +1,12 @@
+package com.wagnqian.girl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirlApplication.class, args);
+    }
+}
